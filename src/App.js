839ce0425b7d25ab2b444280bcc67d706f332a67
skyrim1974 <chrisai36@gmail.com>
2022-02-11 17:48:17 -0500

    added section of content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import AboveTheFold from './components/AboveTheFold';
+import SectionFirst from './components/SectionFirst';
 
 function App() {
   return (
     <div className="resto-one">
 		<Header />
 		<AboveTheFold />
+		<SectionFirst />
     </div>
   );
 }
---
+++ src/components/SectionFirst.js
@@ -0,0 +1,18 @@
+const SectionFirst = () => {
+	return ( 
+		<div className="container-full" id="section-first">
+			<div className="container center">
+				<h2 className="white">Avoid the Stress of Water, Fire, and Mold Damage With Our Property Restoration Services</h2>
+				<p className="light-blue bold">At Restoration 1, we know how good normal feels.</p>
+			
+				<p className="white">We know that when you're dealing with water, mold, or fire damage, nothing feels normal. You have a huge mess to deal with and no easy answers. With one phone call, you’ll connect with our local experts who will help you start a straightforward process to get your property and your life back the way you remember it.</p>
+
+				<p className="white">We aim to make your life as easy as possible during this difficult time of disruption from your daily routine. Dealing with the mess created by water, mold, or fire damage can be frustrating and confusing. Getting back to normal starts with the first call. We will work with you to gather all the details of exactly what’s happened and we’ll dispatch a vehicle to your location immediately. When dealing with a disaster, we know that time is of the essence. We are a restoration company that is here for you 24/7 with Rapid Response.</p>
+
+				<a href="https://reaactjs.org" className="button">Contact Us</a>
+			</div>			
+		</div>
+	 );
+}
+ 
+export default SectionFirst;
